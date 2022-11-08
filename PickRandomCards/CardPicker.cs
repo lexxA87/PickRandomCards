72da96ad475ac702aaf9ce63a962d58c77d1ac63
lexxA87 <alex.o.nikonov@gmail.com>
2022-11-08 22:24:41 +0400

    Added methods RandomSuit and RandomValue for CardPicker class

--- PickRandomCards/CardPicker.cs
+++ PickRandomCards/CardPicker.cs
@@ -2,6 +2,8 @@
 {
     class CardPicker
     {
+        static Random random = new Random();
+
         public static string[] PickSomeCards(int numberOfCards)
         {
             string[] pickedCards = new string[numberOfCards];
@@ -14,11 +16,21 @@
 
         private static string RandomValue()
         {
-            throw new NotImplementedException();
+            int value = random.Next(1, 14);
+            if (value == 1) return "Ace";
+            if (value == 11) return "Jack";
+            if (value == 12) return "Queen";
+            if (value == 13) return "King";
+            return value.ToString();
+
         }
         private static string RandomSuit()
         {
-            throw new NotImplementedException();
+            int value = random.Next(1, 5);
+            if (value == 1) return "Spades";
+            if (value == 2) return "Hearts";
+            if (value == 3) return "Clubs";
+            return "Diamonds";
         }
 
 
